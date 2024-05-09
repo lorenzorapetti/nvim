3405e8c29cd0a5c0d79440e554fb831279183313
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2024-05-09 22:23:40 +0200

    feat: change harpoon keymaps

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -6,41 +6,28 @@ return {
   config = function()
     local harpoon = require('harpoon').setup()
 
-    vim.keymap.set('n', '<leader>h', function()
+    vim.keymap.set('n', '<leader>hh', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end, { desc = 'Open harpoon window' })
 
-    vim.keymap.set('n', '<leader>a', function()
+    vim.keymap.set('n', '<leader>hn', function()
       harpoon:list():add()
-    end, { desc = 'Add current line to harpoon' })
+    end, { desc = 'Add current file to harpoon' })
 
     -- This keymaps are based on ColemakDH
     -- It uses the last row on the right side of the keyboard
-    vim.keymap.set('n', '<A-h>', function()
+    vim.keymap.set('n', '<leader>ha', function()
       harpoon:list():select(1)
     end, { desc = 'Go to harpoon item 1' })
-    vim.keymap.set('n', '<A-,>', function()
+    vim.keymap.set('n', '<leader>hr', function()
       harpoon:list():select(2)
     end, { desc = 'Go to harpoon item 2' })
-    vim.keymap.set('n', '<A-.>', function()
+    vim.keymap.set('n', '<leader>hs', function()
       harpoon:list():select(3)
     end, { desc = 'Go to harpoon item 3' })
-    vim.keymap.set('n', '<A-/>', function()
+    vim.keymap.set('n', '<leader>ht', function()
       harpoon:list():select(4)
     end, { desc = 'Go to harpoon item 4' })
-
-    vim.keymap.set('n', '<leader><A-h>', function()
-      harpoon:list():replace_at(1)
-    end, { desc = 'Add to harpoon item 1' })
-    vim.keymap.set('n', '<leader><A-,>', function()
-      harpoon:list():replace_at(2)
-    end, { desc = 'Add to harpoon item 2' })
-    vim.keymap.set('n', '<leader><A-.>', function()
-      harpoon:list():replace_at(3)
-    end, { desc = 'Add to harpoon item 3' })
-    vim.keymap.set('n', '<leader><A-/>', function()
-      harpoon:list():replace_at(4)
-    end, { desc = 'Add to harpoon item 4' })
 
     -- Toggle previous & next buffers stored within Harpoon list
     -- The keys are based on the homerow of ColemakDH
